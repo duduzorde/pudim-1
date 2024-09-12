108aa45060071e9c530bf569798599c0c6a1bea8
duduzorde <etavaresg@gmail.com>
2024-09-12 01:10:54 -0300

    Puzzles 0.1 + Dialog 0.02
    implementei os 2 puzzles de chave e o puzzle de botoes e corrigi um pobrema q tava dando com os arquivos do diálogo

---
+++ botao_vermelho.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+@export var red_door : Sprite2D
+
+func pressed():
+	frame = 2
+	owner.push_button(get_index())
+
+func unpressed():
+	frame = 1
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pressed()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	unpressed()
