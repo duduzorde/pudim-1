b7fda02ec199ec28b8809de893a4cb382b91a8c1
duduzorde <etavaresg@gmail.com>
2024-09-12 23:10:21 -0300

    AAAAAAAAAAAAAAAAAAAAAAAA

---
+++ chave2.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+var draggable = false
+var is_inside_dropable = false	
+var body_ref
+var offset: Vector2
+var initialPos: Vector2
+var is_dragging = false
+
+func _process(delta):
+	if draggable:
+		if Input.is_action_just_pressed("click"):
+			initialPos = global_position
+			offset = get_global_mouse_position() - global_position
+			is_dragging = true
+		if Input.is_action_pressed("click"):
+			global_position = get_global_mouse_position()
+		elif Input.is_action_just_released("click"):
+			is_dragging = false
+			var tween = get_tree().create_tween()
+			if is_inside_dropable:
+				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(Tween.EASE_OUT)
+				get_tree().change_scene_to_file("res://masmorra_3.tscn")
+				## INSERIR AQUI UMA FUNÇÃO get_tree().change_scene_to_file("res://cena com o bau aberto.tscn") PRA QUANDO TIVER A CENA PRONTA
+			else:
+				tween.tween_property(self,"position",initialPos,0.2).set_ease(Tween.EASE_OUT)
+				
+	if Input.is_action_just_pressed("enter"):
+		get_tree().change_scene_to_file("res://masmorra_3.tscn")
+
+
+
+func _on_area_2d_mouse_entered():
+	if not is_dragging:
+		draggable = true
+		scale = scale * Vector2(1.05, 1.05)
+
+func _on_area_2d_mouse_exited():
+	if not is_dragging:
+		draggable = false
+		scale = scale * Vector2(0.95, 0.95)
+
+
+
+func _on_area_2d_body_entered(body:StaticBody2D):
+	if body.is_in_group('dropable'):
+		is_inside_dropable = true
+		body_ref = body
+	else:
+		is_dragging = false
+		var tween = get_tree().create_tween()
+		tween.tween_property(self,"position",initialPos,0.2).set_ease(Tween.EASE_OUT)
+
+func _on_area_2d_body_exited(body:StaticBody2D):
+	if body.is_in_group('dropable'):
+		is_inside_dropable = false
---
+++ portão.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var player_in_area = false
+
+func _ready():
+	$Label.visible = false  # Esconde o label de interação no início
+
+
+func _process(delta):
+	if player_in_area and Input.is_action_just_pressed("interagir"):  # Verifica se a tecla "E" foi pressionada
+		get_tree().change_scene_to_file("res://puzzle_do_bau_2.tscn")  # Muda para a nova cena
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":  # Verifica se o corpo que entrou é o jogador
+		player_in_area = true
+		$Label.visible = true  # Mostra o label de interação
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		player_in_area = false
+		$Label.visible = false  # Mostra o label de interação
