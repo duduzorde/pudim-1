2349ec538a7c692ba78977d4afd0cab6b69fb18f
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2024-09-12 23:12:59 -0300

    masmorra teste dialogo

--- masmorra_2_teste_dialogo.gd
+++ masmorra_2_teste_dialogo.gd
@@ -4,12 +4,16 @@ extends Node2D
 
 @onready var player = get_node("Player")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func comecaDialogo(timeline: String):
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
-	Dialogic.start('timeline')
+	Dialogic.start(timeline)
 	player.frozen = true
 	get_viewport().set_input_as_handled()
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	comecaDialogo('timeline')
 	pass # Replace with function body.
 
 
@@ -21,3 +25,7 @@ func _on_timeline_ended():
 	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
 	player.frozen = false
 	# do something else here
+
+func _on_area_2d_area_entered(area):
+	comecaDialogo('timeline')
+	pass # Replace with function body.
